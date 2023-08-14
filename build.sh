91c10386474ed9b1c28b24721e5b028e3a3bb8f1
Thouis (Ray) Jones <thouis@broadinstitute.org>
2023-08-14 19:57:25 -0400

    bump docker version

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-gcloud builds submit --timeout=20m --tag us.gcr.io/landerlab-atacseq-200218/donor_assign:0.12
+gcloud builds submit --timeout=20m --tag us.gcr.io/landerlab-atacseq-200218/donor_assign:0.13
